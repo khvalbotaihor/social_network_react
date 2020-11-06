9b2919064cbc742833ac5e46c6d75948ce36ad1e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-06 07:03:44 +0200

    - added new social network components

--- src/App.js
+++ src/App.js
@@ -6,14 +6,34 @@ const App = () => {
     return (
         <div className='app-wrapper'>
             <header className='header'>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTubyb9WhTgpvPRNhyJziWioWCHhAXMgY7zSg&usqp=CAU" alt=""/>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTubyb9WhTgpvPRNhyJziWioWCHhAXMgY7zSg&usqp=CAU"
+                    alt=""/>
             </header>
             <nav className='nav'>
-                <div>Profile</div>
-                <div>Messages</div>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
             </nav>
             <div className='content'>
-                Main content
+                <div>
+                    <img src="https://i.pinimg.com/originals/31/d8/07/31d8075c82022692d2056edebd26d995.jpg" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        <div>post 1</div>
+                        <div>post 2</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
