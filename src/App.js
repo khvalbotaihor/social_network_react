a749408950bc6723f2c27c7681427b25a70721ac
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-06 04:44:51 +0200

    - create new components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className="App">
+        <div>
+            <div><Header /></div>
+            <Technologies />
+                    </div>
+    );
+}
+
+const Technologies = () =>{
+    return (
+        <div >
             <ul>
                 <li>css</li>
                 <li>html</li>
                 <li>js</li>
                 <li>react</li>
             </ul>
+        </div>
+    )
+}
 
+const Header = () => {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
         </div>
     );
 }
