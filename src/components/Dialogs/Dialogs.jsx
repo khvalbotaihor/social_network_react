f8bf3edd232698e0c86597cedd3355884f243561
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-10 05:11:42 +0200

    - 18 point - added additinal styles to parent dialog component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
-
-const Dialogs = (props) =>{
+const Dialogs = (props) => {
     return (
-
+        <div>
+            Dialogs
+        </div>
     )
 }
 
