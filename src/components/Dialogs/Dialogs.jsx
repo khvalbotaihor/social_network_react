b03e93ed68141a3517c559cb13cb61d1a4a00fbf
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-10 16:13:21 +0200

    - 24 point - move dialogsData to a separate file - change urls for dialogitems

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -21,16 +21,30 @@ const Message = (props) => {
 
 
 const Dialogs = (props) => {
+
+    // ето мир данних, ето BLL(DATA)
+    // также у нас есть UI
+    let dialogsData = [
+        {id: 1,name: 'Dimich'},
+        {id: 2,name: 'Andrey'},
+        {id: 3,name: 'Sveta'},
+        {id: 4,name: 'Sasha'},
+        {id: 5,name: 'Victor'},
+        {id: 6,name: 'Valera'},
+        ]
+let dialog = dialogsData.map(e => <DialogItem name={e.name} id={e.id} />)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
+                {dialog}
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
 
-                <DialogItem name="Dimich" id="1"/>
-                <DialogItem name="Andrey" id="2"/>
-                <DialogItem name="Sveta" id="3"/>
-                <DialogItem name="Sasha" id="4"/>
-                <DialogItem name="Victor" id="5"/>
-                <DialogItem name="Valera" id="6"/>
 
             </div>
             <div className={s.messages}>
