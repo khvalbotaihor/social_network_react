022607b03dc52979471b81ac5baa61d8040d9ad1
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-10 07:44:49 +0200

    - 21 point - added dialogs component content - show content

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,19 +7,29 @@ const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <NavLink to="/profile"> Profile </NavLink>
+                <NavLink to="/profile"
+                         activeClassName={classes.activeLink}
+                > Profile </NavLink>
             </div>
             <div className={`${classes.active} ${classes.item}`}>
-                <NavLink to="/dialogs">Messages</NavLink>
+                <NavLink to="/dialogs"
+                         activeClassName={classes.activeLink}
+                >Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/news">News</NavLink>
+                <NavLink to="/news"
+                         activeClassName={classes.activeLink}
+                >News</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="./music">Music</NavLink>
+                <NavLink to="./music"
+                         activeClassName={classes.activeLink}
+                >Music</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/settings">Settings</NavLink>
+                <NavLink to="/settings"
+                         activeClassName={classes.activeLink}
+                >Settings</NavLink>
             </div>
         </nav>
     )
