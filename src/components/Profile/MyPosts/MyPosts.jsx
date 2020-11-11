44d9f73f0d883163bd50746c790b1a5ff08cb1bb
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-11 17:19:59 +0200

    - 31 point - add onclick on addPost button

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,13 +6,15 @@ export const MyPosts = (props) => {
     let postsElement =
         props.posts.map((p, index) => <Post message={p.message} likesCount={p.likesCount} key={index} />)
 
+    let addPost = ()=>{alert("Hey")} ;
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div><textarea></textarea></div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={classes.item}>
