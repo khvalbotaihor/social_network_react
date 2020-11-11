27fdb5a57df820a0e98f1f653fc6ac3fb580f046
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-11 04:55:38 +0200

    - 27 point - replace component attribute with render attribute - add arrow function to render attribute to have an bbility to add props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {Route} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
@@ -12,17 +12,19 @@ import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div class="app-wrapper-content">
-                <Route path="/profile" component={Profile}/>
-                <Route path="/dialogs" component={Dialogs}/>
-                <Route path="/music" component={Music}/>
-                <Route path="/news" component={News}/>
-                <Route path="/settings" component={Settings}/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div class="app-wrapper-content">
+                    <Route path="/profile" render={() => <Profile />}/>
+                    <Route path="/dialogs" render={()=> <Dialogs />}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
