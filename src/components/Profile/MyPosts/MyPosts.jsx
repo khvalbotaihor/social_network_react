f2ba5587d14f1b007b1ca962882b9c77ba13c46b
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-11 04:45:37 +0200

    - 26 point - move posts pre-condition data to an upper level

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import classes from "./MyPosts.module.css";
 import Post from "./Post/MyPosts";
 
-export const MyPosts = () => {
+export const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: "Hi, how are you?", likesCount:12},
-        {id: 2, message: "It's my first post", likesCount:11},
-    ]
+
 
     let postsElement =
-        posts.map((p, index) => <Post message={p.message} likesCount={p.likesCount} key={index}/>)
+        props.posts.map((p, index) => <Post message={p.message} likesCount={p.likesCount} key={index}/>)
 
     return (
         <div className={classes.postsBlock}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,10 +5,18 @@ import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 
 export const Profile = () => {
+    let posts = [
+        {id: 1, message: "Hi, how are you?", likesCount:12},
+        {id: 2, message: "It's my first post", likesCount:11},
+        {id: 3, message: "Blabla", likesCount:11},
+        {id: 4, message: "Dada", likesCount:11},
+    ]
+
+
     return (
         <div className={classes.content}>
             <ProfileInfo />
-            <MyPosts />
+            <MyPosts posts={posts}/>
         </div>
     )
 }
