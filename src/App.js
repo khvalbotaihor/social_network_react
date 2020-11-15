9b3aef3b168522b2b333d9892dc55d4fc78d1e69
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-15 08:15:24 +0200

    - 44 point - added <StoreContext.Provider value={store}></StoreContext.Provider> that overlaps app component - remove props from app component
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
@@ -12,7 +11,6 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
     return (
-        <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar friends={props.state.sidebar}/>
@@ -26,7 +24,6 @@ const App = (props) => {
                     <Route path="/settings" component={Settings}/>
                 </div>
             </div>
-        </BrowserRouter>
     );
 }
 
