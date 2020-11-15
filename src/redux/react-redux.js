ef49c0ab2dfb7c6b6ba8a7f996ce06efd4e2e5ad
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-15 04:51:46 +0200

    - 42 point - ceated redux store combine reducers
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

---
+++ src/redux/react-redux.js
@@ -0,0 +1,12 @@
+import {createStore} from "redux";
+import sidebarReducer from "./sidebar-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+
+let reducer = ({
+    profilePage:profileReducer,
+    dialogsPage:dialogsReducer,
+    sidebar: sidebarReducer
+})
+
+let store = createStore();
