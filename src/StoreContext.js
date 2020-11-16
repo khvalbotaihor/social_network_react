f55a2a5c81960c772150d9f0494ab1e180fc6c0c
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-16 04:42:08 +0200

    - 44 point - replace storecontext component with provider component
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/StoreContext.js
+++ src/StoreContext.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import store from "./redux/react-redux";
-import {BrowserRouter} from "react-router-dom";
 
 const StoreContext = React.createContext(null);
 
-const Provider = (props) => {
+export const Provider = (props) => {
     return <StoreContext.Provider value={props.store}>
         {props.children}
     </StoreContext.Provider>
     }
 
-export default StoreContext;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-//import store from "./redux/state";
 import store from "./redux/react-redux";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import StoreContext from "./StoreContext";
+import {Provider} from "./StoreContext";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <StoreContext.Provider value={store}>
+                <Provider store={store}>
                     <App />
-                </StoreContext.Provider>
+                </Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
