a7efdcc66f36fba63ae1343f9ddf1cfe369c1f60
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-16 04:25:39 +0200

    - 44 point - created store context component to incapsulate storecontext variable
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/StoreContext.js
+++ src/StoreContext.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import store from "./redux/react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 const StoreContext = React.createContext(null);
+
+const Provider = (props) => {
+    return <StoreContext.Provider value={props.store}>
+        {props.children}
+    </StoreContext.Provider>
+    }
 
 export default StoreContext;
