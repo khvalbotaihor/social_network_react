7cae64f6e96b5de80eb9f74631af7f6521470b75
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-16 05:47:27 +0200

    - 45 point - implement react-redux container instead redux provider
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,34 +1,27 @@
 import React from 'react';
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
-import StoreContext from "../../StoreContext";
-
-const DialogsContainer = () => {
-    return (
-
-        <StoreContext.Consumer>{(store) => {
-
-            let state = store.getState().dialogsPage;
-
-            const SendMessage = () => {
-                store.dispatch(sendMessageCreator())
-            }
-
-            let updateNewMessageBody = (body) => {
-                store.dispatch(updateNewMessageBodyCreator(body))
-            }
-
-            return (
-                <Dialogs
-                    SendMessage={SendMessage}
-                    updateNewMessageBody={updateNewMessageBody}
-                    dialogsPage={state}/>
-
-            )
-        }}</StoreContext.Consumer>
+import {connect} from "react-redux";
 
 
-    )
+let mapSateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
 }
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody: (body) => {
+            dispatch(updateNewMessageBodyCreator(body))
+        },
+        SendMessage: () => {
+            dispatch(sendMessageCreator())
+        }
+    }
+}
+
+const DialogsContainer = connect(mapSateToProps, mapDispatchToProps)(Dialogs)
+
 
 export default DialogsContainer;
