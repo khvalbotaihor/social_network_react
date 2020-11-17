208f496679c75893b7648c1b1a084808408a8d32
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-17 17:22:58 +0200

    - 52 point - replace functional component with class component - use constructor to send requests to server
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import styles from './users.module.css'
+import userPhoto from "../../assets/user.png";
+import styles from "./users.module.css";
 import * as axios from "axios";
-import userPhoto from '../../assets/user.png';
 
-const Users = (props) => {
-    let getUsers = () =>{
-        if (props.users.length === 0) {
+
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
             axios.get("https://social-network.samuraijs.com/api/1.0/users")
                 .then(response => {
-                    props.setUsers(response.data.items)
+                    this.props.setUsers(response.data.items)
                 });
-        }
     }
 
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
-            {props.users.map(u => <div key={u.id}>
+
+    render() {
+        return (
+            <div>
+                {this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
@@ -24,15 +26,15 @@ const Users = (props) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -42,10 +44,12 @@ const Users = (props) => {
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)}
-        </div>
-    );
-
+                </div>)}
+            </div>
+        );
+    }
 }
+
+
 
 export default Users;
