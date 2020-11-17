fc7749f273fe539e858f59b873c417ecdda11ce6
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-17 04:47:55 +0200

    - 48 point - refactor profile reducer deep copy logic - update deep logic copy a little bit
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -13,28 +13,30 @@ const initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:{
+        case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             }
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText ="";
-            return stateCopy;
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText : ""
+            };
         }
-        case UPDATE_NEW_POST_TEXT:{
-            let stateCopy = {...state};
-            stateCopy.newPostText = action.newText
-            return stateCopy;}
+        case UPDATE_NEW_POST_TEXT: {
+            return {
+                ...state,
+                newPostText: action.newText
+            }
+        }
         default:
             return state;
     }
 }
 
 
-    export const addPostActionCreator = () => ({type: ADD_POST})
-    export const updateNewPostTextActionCreator = (value) => ({type: UPDATE_NEW_POST_TEXT, newText: value})
-    export default profileReducer;
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (value) => ({type: UPDATE_NEW_POST_TEXT, newText: value})
+export default profileReducer;
