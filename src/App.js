84c3003371bd1317e4266382c080d4142bd3325e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-17 06:39:54 +0200

    - 49 point - add container component for users component - add mapToProps function - add mapDispatchToProps functions - add connect function
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props) => {
     return (
@@ -20,7 +20,7 @@ const App = (props) => {
                            render={() => <DialogsContainer /> }/>
                     <Route path="/profile"
                            render={() => <Profile />}/>
-                    <Route path="/users" render={()=><Users /> }/>
+                    <Route path="/users" render={()=><UsersContainer /> }/>
                     <Route path="/music" component={Music}/>
                     <Route path="/news" component={News}/>
                     <Route path="/settings" component={Settings}/>
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import {createStore, combineReducers} from 'redux';
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
+import usersReducer from "./users-reducer";
 
 let reducers = combineReducers({
     profileReducer: profileReducer,
     dialogsReducer: dialogsReducer,
-    sidebarReducer: sidebarReducer
+    sidebarReducer: sidebarReducer,
+    usersPage: usersReducer
 });
 
 
