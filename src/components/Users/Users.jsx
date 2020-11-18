61e276a1a8343729c05bdcf4914b23d0a92d172c
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-18 17:32:19 +0200

    - 59 point - added Navlink to a users icon image
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import userPhoto from "../../assets/user.png";
 import styles from "./users.module.css";
+import {NavLink} from "react-router-dom";
 
 let Users = (props) => {
 
@@ -27,7 +28,9 @@ let Users = (props) => {
             {props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
+                        <NavLink to={'/profile/' + u.id}>
+                             <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
+                        </NavLink>
                     </div>
                     <div>
                         {u.followed
