d09e3fe4b9e85bc76bdc875182272172b1252dbf
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-18 06:28:12 +0200

    - 55 point - added logc pagination
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -12,14 +12,14 @@ class Users extends React.Component {
     // component will unmount
 
 componentDidMount() {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
         .then(response => {
             this.props.setUsers(response.data.items)
         });
 }
 
     render() {
-    let pagesCount = this.props.totalUsersCount / this.props.pageSize;
+    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
 
     let pages = [];
     for (let i =1; i <=pagesCount; i++){
@@ -30,7 +30,11 @@ componentDidMount() {
             <div>
                 <div>
                     {pages.map(p =>{
-                        return <span className={this.props.currentPage === p && styles.selectedPage}>{p}</span> })
+                        return <span
+                            onClick={()=>{
+                                this.props.setCurrentPage(p)
+                            }}
+                            className={this.props.currentPage === p && styles.selectedPage}>{p}</span> })
                     }
                 </div>
                 {this.props.users.map(u => <div key={u.id}>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 import Users from "./Users";
 
 let mapStateToProps = (state) =>{
@@ -15,7 +15,8 @@ let mapDispatchToProps = (dispatch) =>{
     return {
         follow: (userId)=>dispatch(followAC(userId)),
         unfollow: (userId)=>dispatch(unfollowAC(userId)),
-        setUsers: (users)=>dispatch(setUsersAC(users))
+        setUsers: (users)=>dispatch(setUsersAC(users)),
+        setCurrentPage: (pageNumber)=>dispatch(setCurrentPageAC(pageNumber))
     }
 }
 
