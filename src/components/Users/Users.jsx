47cb285290731068a32161135defb120649b7529
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-18 06:59:16 +0200

    - 55 point - fix server pagination functionality
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -11,30 +11,41 @@ class Users extends React.Component {
     // component will mount
     // component will unmount
 
-componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-        .then(response => {
-            this.props.setUsers(response.data.items)
-        });
-}
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
+    }
 
     render() {
-    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
 
-    let pages = [];
-    for (let i =1; i <=pagesCount; i++){
-        pages.push(i);
-    };
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+        ;
 
         return (
             <div>
                 <div>
-                    {pages.map(p =>{
+                    {pages.map(p => {
                         return <span
-                            onClick={()=>{
-                                this.props.setCurrentPage(p)
+                            onClick={(e) => {
+                                this.onPageChanged(p)
                             }}
-                            className={this.props.currentPage === p && styles.selectedPage}>{p}</span> })
+                            className={this.props.currentPage === p && styles.selectedPage}>{p}</span>
+                    })
                     }
                 </div>
                 {this.props.users.map(u => <div key={u.id}>
@@ -68,7 +79,6 @@ componentDidMount() {
         );
     }
 }
-
 
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {followAC, setCurrentPageAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
 import Users from "./Users";
 
 let mapStateToProps = (state) =>{
@@ -16,7 +16,8 @@ let mapDispatchToProps = (dispatch) =>{
         follow: (userId)=>dispatch(followAC(userId)),
         unfollow: (userId)=>dispatch(unfollowAC(userId)),
         setUsers: (users)=>dispatch(setUsersAC(users)),
-        setCurrentPage: (pageNumber)=>dispatch(setCurrentPageAC(pageNumber))
+        setCurrentPage: (pageNumber)=>dispatch(setCurrentPageAC(pageNumber)),
+        setTotalUsersCount: (totalCount)=>dispatch(setTotalUsersCountAC(totalCount))
     }
 }
 
