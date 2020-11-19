fcb0aba7d7dac626aafb330a13cc9e58ae0b14f8
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-19 15:42:27 +0200

    - 63 point - moved withCredentials and headers to an axios variable for all requests
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/api/api.js
+++ src/api/api.js
@@ -29,22 +29,12 @@ export const getUser = (userId) =>{
 }
 
 export const followUser = (id) =>{
-    return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "4d139ff6-a913-4dd1-9885-7cbd4a942daa"
-        }
-    }).then(response =>{
+    return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`).then(response =>{
         return response.data
     })
 }
 export const unFollowUser = (id) =>{
-    return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "4d139ff6-a913-4dd1-9885-7cbd4a942daa"
-        }
-    }).then(response =>{
+    return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`).then(response =>{
         return response.data
     })
 }
