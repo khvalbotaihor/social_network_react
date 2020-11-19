718c764daa7ac729c76b289fa46400cd70bf40b1
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-19 15:45:57 +0200

    - 63 point - moved baseUrl to s separate file
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/api/api.js
+++ src/api/api.js
@@ -2,39 +2,40 @@ import * as axios from "axios";
 
 const instance = axios.create({
     withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
     headers: {
         "API-KEY": "4d139ff6-a913-4dd1-9885-7cbd4a942daa"
     }
-
 })
 
 
-export const getUsers = (currentPage =1,pageSize =10) =>{
-    return instance.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`)
+export const getUsers = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => {
-            return response.data})
-}
-
-export const authMe = () =>{
-    return  instance.get(`https://social-network.samuraijs.com/api/1.0/auth/me`).then(response => {
-        return response.data
-    })
-}
-
-export const getUser = (userId) =>{
-    return  instance.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
-        .then(response =>{
             return response.data
         })
 }
 
-export const followUser = (id) =>{
-    return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`).then(response =>{
+export const authMe = () => {
+    return instance.get(`auth/me`).then(response => {
         return response.data
     })
 }
-export const unFollowUser = (id) =>{
-    return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`).then(response =>{
+
+export const getUser = (userId) => {
+    return instance.get(`profile/` + userId)
+        .then(response => {
+            return response.data
+        })
+}
+
+export const followUser = (id) => {
+    return instance.post(`follow/${id}`).then(response => {
+        return response.data
+    })
+}
+export const unFollowUser = (id) => {
+    return instance.delete(`follow/${id}`).then(response => {
         return response.data
     })
 }
