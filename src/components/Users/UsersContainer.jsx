58dffd420593ce7c19c7fbc5883f222855a172e8
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-19 14:22:49 +0200

    - 63 point - moved getUsers api request to a separate file
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -11,13 +11,18 @@ import {
 import * as axios from "axios";
 import Users from "./Users";
 import Preloader from '../Common/Preloader/preloader';
+import {getUsers} from "../../api/api";
 
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
+
+
+
+/*        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
             withCredentials: true
-        })
+        })*/
+        getUsers(this.props.currentPage,this.props.pageSize)
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items)
@@ -28,9 +33,7 @@ class UsersContainer extends React.Component {
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,{
-            withCredentials: true
-        })
+        getUsers(pageNumber,this.props.pageSize)
             .then(response => {
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items)
