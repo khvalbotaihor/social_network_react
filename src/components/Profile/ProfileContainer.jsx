e367661ea598d7dfa67b8c2688b25102369368e8
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-20 11:53:05 +0200

    - 69 point - created auth redirect component
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -16,8 +16,6 @@ class ProfileContainer extends React.Component {
     }
 
     render() {
-        if (!this.props.isAuth) return <Redirect to="/login"/>
-
         return <Profile
             {...this.props}
             profile={this.props.profile}
@@ -25,12 +23,17 @@ class ProfileContainer extends React.Component {
     }
 }
 
+let AuthRedirectComponent = (props) => {
+    if (!this.props.isAuth) return <Redirect to="/login"/>
+    return <ProfileContainer {...props}/>
+}
+
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
     isAuth: state.auth.isAuth
 })
 
-let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(AuthRedirectComponent);
 
 
 export default connect(mapStateToProps, {getUserProfile})(WithUrlDataContainerComponent);
