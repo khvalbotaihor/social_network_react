950d2049183560d268b1fe598255a693bdd7029e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-25 06:40:12 +0200

    - 77 point - created formsControls component - replace tag representing in form to our new formsControls component
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

---
+++ src/components/Common/FormsControls/FormsControls.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TextArea = (props) =>{
+    return (
+        <div>
+            <textarea {...props} />
+        </div>
+    )
+}
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import classes from "./MyPosts.module.css";
 import Post from "./Post/MyPosts";
 import {Field, reduxForm} from "redux-form";
+import { maxLengthCreator, required} from "../../../utils/validators/validators";
+import {TextArea} from "../../Common/FormsControls/FormsControls";
+
+const maxLength10 = maxLengthCreator(10);
+
 
 export const MyPosts = (props) => {
     let postsElement =
         props.posts.map((p, index) => <Post message={p.message} likesCount={p.likesCount} key={index}/>)
 
     let onAddPost = (value) => {
+        debugger;
         props.addPost(value.NewPostText);
 
     }
+
 
     return (
 
@@ -33,7 +40,8 @@ const AddNewPostForm = (props) => {
                     <Field
                         placeholder="Add new post"
                         name="NewPostText"
-                        component="textarea"
+                        component={TextArea}
+                        validate={[required, maxLength10]}
 
                     />
             </div>
