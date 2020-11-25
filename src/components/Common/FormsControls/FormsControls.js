45d951ebce53f5de5c6589ea06dcf4431dbad73c
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-25 07:03:36 +0200

    - 77 point - added error css and apply it to the component
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Common/FormsControls/FormsControls.js
+++ src/components/Common/FormsControls/FormsControls.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styles from './FormsControls.module.css'
 
 export const TextArea = ({input, meta, ...props}) =>{
     return (
-        <div>
+        <div className={styles.formControl + " " + styles.error}>
             <textarea {...input} {...props} />
         </div>
     )
