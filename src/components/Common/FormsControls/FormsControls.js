d4c51c266b4c36d528419570cd588ab6a5248bf6
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-26 13:39:04 +0200

    - 77 point - added validation to login page
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Common/FormsControls/FormsControls.js
+++ src/components/Common/FormsControls/FormsControls.js
@@ -2,13 +2,27 @@ import React from 'react';
 import styles from './FormsControls.module.css'
 
 export const TextArea = ({input, meta, ...props}) => {
-
     const hasError = meta.touched && meta.error;
 
     return (
         <div className={styles.formControl + " " + (hasError ? styles.error : "") }>
             <div>
                 <textarea {...input} {...props} />
+            </div>
+            <span>
+                {hasError && meta.error}
+            </span>
+        </div>
+    )
+}
+
+export const Input = ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error;
+
+    return (
+        <div className={styles.formControl + " " + (hasError ? styles.error : "") }>
+            <div>
+                <input {...input} {...props} />
             </div>
             <span>
                 {hasError && meta.error}
