62f2d4cbfc49fcea56df87eab111aed635d1abd5
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-27 09:58:23 +0200

    - 77 point - created component to unify code styles
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Common/FormsControls/FormsControls.js
+++ src/components/Common/FormsControls/FormsControls.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './FormsControls.module.css'
 
-export const TextArea = ({input, meta, ...props}) => {
+const FormControl = ({input, meta, child, ...props}) => {
     const hasError = meta.touched && meta.error;
 
     return (
-        <div className={styles.formControl + " " + (hasError ? styles.error : "") }>
+        <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
             <div>
-                <textarea {...input} {...props} />
+                {props.children}
             </div>
             <span>
                 {hasError && meta.error}
@@ -16,17 +16,17 @@ export const TextArea = ({input, meta, ...props}) => {
     )
 }
 
-export const Input = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error;
-
+export const TextArea = (props) => {
+const {input, meta, child, ...restProps} =props
     return (
-        <div className={styles.formControl + " " + (hasError ? styles.error : "") }>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            <span>
-                {hasError && meta.error}
-            </span>
-        </div>
+        <FormControl {...props}> <input {...input} {...restProps} /> </FormControl>
+    )
+}
+
+export const Input = (props) => {
+    const {input, meta, child, ...restProps} =props
+    return (
+        <FormControl {...props}> <input {...input} {...restProps} /> </FormControl>
+
     )
 }
