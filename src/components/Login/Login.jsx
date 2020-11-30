f6b49080d683e4c8eddf126b9fbc328d8f7f094b
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-30 15:27:00 +0200

    - 78 point - add mapStateToProps
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,13 +4,14 @@ import {Input} from "../Common/FormsControls/FormsControls";
 import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
+import {Redirect} from "react-router-dom";
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
                 <Field placeholder={"Login"} name={"email"} component={Input}
-                    validate={[required]}
+                       validate={[required]}
                 />
             </div>
             <div>
@@ -37,6 +38,9 @@ const Login = (props) => {
         debugger;
         props.login(formData.email, formData.password, formData.rememberMe)
     }
+    if (props.isAuth) {
+        return <Redirect to={"/profile"}/>
+    }
 
     return <div>
         <h1>LOGIN</h1>
@@ -44,5 +48,8 @@ const Login = (props) => {
     </div>
 }
 
+const mapStateToProps=(state) =>({
+    isAuth: state.auth.isAuth
+})
 
-export default connect(null, {login})(Login);
+export default connect(mapStateToProps, {login})(Login);
