d5fe8a3b91c35109ccb1c4e3a3a6381b37fe2355
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-02 16:27:33 +0200

    - 84 point - created StatusInfo functional component
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import s from './ProfileInfo.module.css';
 import Preloader from "../../../components/Common/Preloader/preloader"
 import {NavLink, Route} from "react-router-dom";
 import ProfileStatus from "./ProfileStatus"
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile){
@@ -15,7 +16,7 @@ const ProfileInfo = (props) => {
             </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
 
                 <hr/>
                 <div>About me: {props.profile.aboutMe}</div>
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,75 @@
+import React, {useState} from "react";
+import s from './ProfileInfo.module.css';
+
+const ProfileStatusWithHooks = (props) => {
+   const [editMode, setEditMode] = useState(false);
+   const [status, setStatus] = useState(props.status);
+
+
+
+   const  activateEditMode = () =>{
+       setEditMode(true)
+   }
+   const deActivateEditMode = () =>{
+       setEditMode(false);
+       props.updateStatus(status)
+   }
+
+   const onStatusChange = (e) =>{
+       setStatus(e.currentTarget.value)
+   }
+
+
+
+
+/*
+    componentDidUpdate(prevProps,prevState ) {
+        if (prevProps.status !== this.props.status){
+            this.setState({
+                status: this.props.status
+            })
+        }
+
+        let a = this.state;
+        let b = this.props;
+
+        console.log("componentDidUpdate")
+    }*/
+
+        console.log("render")
+        return (
+            <div>
+                {editMode
+                    ? <div>
+                        <input
+                            value={status}
+                            autoFocus={true}
+                            onBlur={deActivateEditMode}
+                            onChange={onStatusChange}
+                        ></input>
+                    </div>
+                    : <div>
+                        <span onDoubleClick={activateEditMode}>{ props.status || "-----"}</span>
+                    </div>
+                }
+
+            </div>
+/*    <div>
+                {this.state.editMode
+                    ? <div>
+                        <input autoFocus={true}
+                               onBlur={this.deactivateEditMode}
+                               value={this.state.status}
+                               onChange={this.onStatusChange}
+                        ></input>
+                    </div>
+                    : <div>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status || "-----"}</span>
+                    </div>
+                }
+
+            </div>*/
+        )
+}
+
+export default ProfileStatusWithHooks;
