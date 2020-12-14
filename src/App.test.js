a0d335e775296c03a7bec4e247ca0f41eccb9103
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-14 18:16:15 +0200

    - 92 point - update App unit test
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
 import App from './App';
 import ReactDOM from 'react-dom';
 import React from "react";
+import SamuraiJSApp from "./App";
 
 
 test('renders learn react link', () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<SamuraiJSApp />, div)
 
   ReactDOM.unmountComponentAtNode(div)
 
