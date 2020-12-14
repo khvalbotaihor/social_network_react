bd2ddfe11e71c2cbd3e3da4d974e9810a7424d60
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-14 23:01:48 +0200

    - 92 point - check inout edit mode test
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -26,8 +26,20 @@ describe("ProfileStatus component", () => {
 
     test("after creation <span> should contain correct status", () => {
         const component = create(<ProfileStatus status={"it-kamasutra.com"} />);
-        const root = component.root();
-        let span = instance.findByType("span");
+        const root = component.root;
+        let span = root.findByType("span");
         expect(span.children[0]).toBe("it-kamasutra.com");
     });
+
+    test("input should be displayed in editMode instead of span", () => {
+        const component = create(<ProfileStatus status={"it-kamasutra.com"} />);
+        const root = component.root;
+        let span = root.findByType("span");
+        span.props.onDoubleClick();
+
+        let input = root.findByType("input");
+        expect(input.props.value).toBe("it-kamasutra.com");
+    });
+
+
 });
