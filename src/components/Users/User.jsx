430820c4d9dc7d5f7204196967dc8be486d5c256
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-14 16:20:55 +0200

    - 90 point - created separate User component
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import userPhoto from "../../assets/user.png";
+import styles from "./users.module.css";
+import {NavLink} from "react-router-dom";
+
+let User = ({user, followingInProgress, unFollow, follow}) => {
+    let u = user;
+    return (
+        <div>
+                <span>
+                    <div>
+                        <NavLink to={'/profile/' + u.id}>
+                             <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                                  className={styles.userPhoto}/>
+                        </NavLink>
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button disabled={followingInProgress.some(id => id === u.id)} onClick={() => {
+                                unFollow(u.id)
+                            }}>
+                                Unfollow</button>
+                            : <button disabled={followingInProgress.some(id => id === u.id)} onClick={() => {
+                                follow(u.id)
+                            }}>Follow</button>}
+                    </div>
+                </span>
+            <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
+                </span>
+        </div>
+    );
+}
+
+
+export default User;
