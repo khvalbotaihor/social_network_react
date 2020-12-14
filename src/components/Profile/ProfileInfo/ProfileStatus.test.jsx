7e9e0ea9409e245cce914667c80ad450b49591e7
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-14 23:08:50 +0200

    - 92 point - check callback function
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -41,5 +41,15 @@ describe("ProfileStatus component", () => {
         expect(input.props.value).toBe("it-kamasutra.com");
     });
 
+    test("callback should be called", () => {
+        const mockCallback = jest.fn();
+        const component = create(<ProfileStatus status={"it-kamasutra.com"} updateStatus={mockCallback} />);
+        const instance = component.getInstance();
+
+        instance.deactivateEditMode();
+
+        expect(mockCallback.mock.calls.length).toBe(1);
+    });
+
 
 });
