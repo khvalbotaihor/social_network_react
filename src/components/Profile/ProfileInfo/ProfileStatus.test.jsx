b6a1db1739baf9dcebfb53f399dd931f25ab0aeb
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-14 22:50:04 +0200

    - 92 point - create test rerender tests
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { create } from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
 
-describe("Button component", () => {
-    test("it shows the expected text when clicked (testing the wrong way!)", () => {
-        const component = create(<Button text="SUBSCRIBE TO BASIC" />);
+describe("ProfileStatus component", () => {
+    test("status from props should be in state", () => {
+        const component = create(<ProfileStatus status={"it-kamasutra.com"} />);
         const instance = component.getInstance();
-        expect(instance.state.text).toBe("");
+        expect(instance.state.status).toBe("it-kamasutra.com");
+    });
+
+    test("after creation <span> with status should be displayed", () => {
+        const component = create(<ProfileStatus status={"it-kamasutra.com"} />);
+        const root = component.root;
+        let span = root.findByType("span");
+        expect(span).not.toBeNull();
+    });
+
+    test("after creation <span> should contain correct status", () => {
+        const component = create(<ProfileStatus status={"it-kamasutra.com"} />);
+        const root = component.root();
+        let span = instance.findByType("span");
+        expect(span.innerText).toBe("it-kamasutra.com");
     });
 });
