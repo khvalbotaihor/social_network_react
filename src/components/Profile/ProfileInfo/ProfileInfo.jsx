bad97c2accbc7801f708a9fb9bab66cb6cb5eb33
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-15 22:13:06 +0200

    - 96 point - add onChange function to input element
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -10,11 +10,16 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner}) => {
     if (!profile){
         return <Preloader />
     }
+
+    const onMainPhotoSelected = (e) =>{
+
+    }
+
     return (
         <div >
             <div className={s.descriptionBlock}>
                 <img src={profile.photos.large || userPhoto } className={s.mainPhoto}/>
-                {isOwner && <input type={"file"}/>}
+                {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
 
                 <hr/>
