cdb2c31120c630e551cf893467af9a93e07dff02
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-12-15 09:43:59 +0200

    - 92 point - added paginator second unit test
    Signed-off-by: Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>

--- src/components/Common/Paginator/Paginator.test.jsx
+++ src/components/Common/Paginator/Paginator.test.jsx
@@ -10,6 +10,13 @@ describe("Paginator component test", ()=>{
         expect(span.length).toBe(10);
     })
 
+    test("if pages count is more then 10, button Next should be present", ()=>{
+        const component = create(<Paginator totalItemsCount={11} pageSize={1} portionSize={10}/>)
+        const root = component.root;
+        let button = root.findAllByType("button");
+        expect(button.length).toBe(1);
+    })
+
 
 
 
